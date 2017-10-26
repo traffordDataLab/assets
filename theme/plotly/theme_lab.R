643a6adb0799cbc6e452a0c2cfb91a58e4a33033
Henry Partridge <hcpartridge@uclmail.net>
2017-10-26 09:51:53 +0100

    Create theme_lab.R

---
+++ theme/plotly/theme_lab.R
@@ -0,0 +1,34 @@
+plot.title <- list(
+  family = "Roboto",
+  size = 18,
+  color = "#757575")
+
+plot.subtitle <- list(
+  family = "Roboto",
+  size = 12,
+  color = "#757575")
+
+plot.caption <- list(
+  family = "Roboto",
+  size = 10,
+  color = "#212121")
+
+axis.title <- list(
+  family = "Open Sans",
+  size = 11,
+  color = "#757575")
+
+axis.text <- list(
+  family = "Open Sans",
+  size = 10,
+  color = "#757575")
+
+legend.title <- list(
+  family = "Roboto",
+  size = 12,
+  color = "#757575")
+
+legend.text <- list(
+  family = "Roboto",
+  size = 10,
+  color = "#757575")
