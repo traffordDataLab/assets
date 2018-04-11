cf44dd1b5ba71fef386116041c408fd13351c72f
James Austin <james.austin@trafford.gov.uk>
2018-04-11 12:26:52 +0100

    Allowed caller to pass a string to search rather than only the URL and changed the decode function to decodeURIComponent so that ampersands etc. within strings are properly decoded.

--- javascript/labGetQryStrValByKey.js
+++ javascript/labGetQryStrValByKey.js
@@ -1,10 +1,12 @@
 /*
     Created:        2018/04/05 by James Austin - Trafford Data Lab
-    Purpose:        To obtain data passed via a query string in the URL
+    Purpose:        To obtain data passed via a query string in the URL or an optional string
     Dependencies:   None
     Licence:        http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/
 */
-function labGetQryStrValByKey(key) {
-    var qsVal = decodeURI((RegExp(key + '=' + '(.+?)(&|$)').exec(location.search) || [, ])[1]);
+function labGetQryStrValByKey(key, str) {
+    var def_str = (str === null) ? location.search : str;   // unless we have been given a specific string, search the URL query string for the key
+
+    var qsVal = decodeURIComponent((RegExp(key + '=' + '(.+?)(&|$)').exec(def_str) || [, ])[1]);
     return (qsVal === "undefined") ? null : qsVal;
 }
