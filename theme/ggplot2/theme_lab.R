dfe177c08d638b2c341742afab58a0285ba03a2e
Henry Partridge <henry.partridge@trafford.gov.uk>
2019-10-03 11:00:23 +0100

    Update theme_lab.R

--- theme/ggplot2/theme_lab.R
+++ theme/ggplot2/theme_lab.R
@@ -1,32 +1,36 @@
 theme_lab <- function () { 
-  theme_grey(base_size = 11.5, base_family = "Roboto") %+replace% 
+  theme_grey(base_size = 14, base_family = "sans") %+replace% 
     theme(
-      # add padding to the plot
+      # plot margin
       plot.margin = unit(rep(0.5, 4), "cm"),
-      # remove the plot background and border
+      # plot background and border
       plot.background = element_blank(),
       panel.background = element_blank(),
       panel.border = element_blank(),
-      # make the legend and strip background transparent
-      legend.background = element_rect(fill = "transparent", colour = NA),
-      legend.key = element_rect(fill = "transparent", colour = NA),
-      strip.background = element_rect(fill = "transparent", colour = NA),
-      # add light, dotted major grid lines only
-      panel.grid.major = element_line(linetype = "dotted", colour = "#757575", size = 0.3),
+      # grid lines
+      panel.grid.major.x = element_blank(),
+      panel.grid.major.y = element_line(color = "#cbcbcb", size = 0.5), 
       panel.grid.minor = element_blank(),
-      # remove the axis tick marks and hide axis lines
+      # axis ticks and lines
       axis.ticks = element_blank(),
-      axis.line = element_line(color = "#FFFFFF", size = 0.3),
-      # modify the bottom margins of the title and subtitle
-      plot.title = element_text(size = 18, colour = "#757575", hjust = 0, margin = margin(b = 4)),
-      plot.subtitle = element_text(size = 12, colour = "#757575", hjust = 0, margin = margin(b = 10)),
-      # add padding to the caption
-      plot.caption = element_text(size = 10, colour = "#212121", hjust = 1, margin = margin(t = 15)),
-      # change to Open Sans for axes titles, tick labels, legend title and legend key, and strip text
-      axis.title = element_text(family = "Open Sans", size = 11, colour = "#757575", face = "plain", hjust = 1),
-      axis.text = element_text(family = "Open Sans", size = 10, colour = "#757575", face = "plain"),
+      axis.line = element_blank(),
+      # title,  subtitle and caption
+      plot.title = element_text(size = 20, face = "bold", colour = "#757575", hjust = 0),
+      plot.subtitle = element_text(size = 16, colour = "#757575", hjust = 0, margin = margin(9, 0, 9, 0)),
+      plot.caption = element_text(size = 10, color = "grey50", hjust = 1, margin = margin(t = 15)),
+      # axes titles
+      axis.title = element_text(colour = "#757575", hjust = 1),
+      axis.text.x = element_text(margin = margin(b = 7)),
+      axis.text.y = element_text( margin = margin(l = 7)),
+      # legend
+      legend.position = "top",
+      legend.background = element_blank(),
+      legend.key = element_blank(),
       legend.title = element_text(size = 12, colour = "#757575"),
+      legend.text.align = 0,
       legend.text = element_text(size = 10, colour = "#757575"),
-      strip.text = element_text(family = "Open Sans", size = 12, colour = "#757575", face = "plain")
+      # facetting
+      strip.background = element_rect(fill = "transparent", colour = NA),
+      strip.text = element_text(size = 12, colour = "#757575", face = "bold", hjust = 0)
     )
 }
